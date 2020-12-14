4b09ac8b002af820bb5055ffce2d66f599d0cbea
Tobias Molenaar <tobias.molenaar@deltares.nl>
2020-12-14 09:11:50 +0100

    gdal

--- SimpleCode.py
+++ SimpleCode.py
@@ -1,3 +1,9 @@
-x = 10
-for i in range(x):
-  print(i)
+from osgeo import gdal
+
+filepath = r"P:\11202428-hisea-inter\Tobias_Molenaar\01-Data\DAP\Data\SW00_OPT_MS4_1C_20180612T092821_20180612T092823_TOU_1234_a2dc.DIMA\IMG_SW00_OPT_MS4_1C_20180612T092821_20180612T092823_TOU_1234_a2dc_R1C1.TIF"
+
+# Open the file:
+raster = gdal.Open(filepath)
+
+# Check type of the variable 'raster'
+type(raster)
