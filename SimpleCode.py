9e5147928cc910f1d7ddd790a4371c72fe796706
Tobias Molenaar <tobias.molenaar@deltares.nl>
2020-12-14 11:11:15 +0100

    variables

--- SimpleCode.py
+++ SimpleCode.py
@@ -1,10 +1,13 @@
-folder = "C:\Users\molenaar\OneDrive - Stichting Deltares\Documents\Thesis - Deltares\Data"
+from netCDF4 import Dataset
+
+folder = r"C:\Users\molenaar\OneDrive - Stichting Deltares\Documents\Thesis - Deltares\Data"
 name = "IMG_SW00_OPT_MS4_1C_20180612T092821_20180612T092823_TOU_1234_a2dc_R1C1"
 
 file = folder + "/" + name + ".nc"
 dataset = Dataset(file, "r")
+lst = list(dataset.variables.keys())
+
 var_values = {}
 var_units = {}
 var_names = {}
-lst = list(dataset.variables.keys())
 print(lst)
