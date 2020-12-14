76673fdba74f86ea231c0017920ff8e55a654030
Tobias Molenaar <tobias.molenaar@deltares.nl>
2020-12-14 11:07:03 +0100

    open netCDF

--- SimpleCode.py
+++ SimpleCode.py
@@ -1,9 +1,10 @@
-from osgeo import gdal
+folder = "C:\Users\molenaar\OneDrive - Stichting Deltares\Documents\Thesis - Deltares\Data"
+name = "IMG_SW00_OPT_MS4_1C_20180612T092821_20180612T092823_TOU_1234_a2dc_R1C1"
 
-filepath = r"P:\11202428-hisea-inter\Tobias_Molenaar\01-Data\DAP\Data\SW00_OPT_MS4_1C_20180612T092821_20180612T092823_TOU_1234_a2dc.DIMA\IMG_SW00_OPT_MS4_1C_20180612T092821_20180612T092823_TOU_1234_a2dc_R1C1.TIF"
-
-# Open the file:
-raster = gdal.Open(filepath)
-
-# Check type of the variable 'raster'
-type(raster)
+file = folder + "/" + name + ".nc"
+dataset = Dataset(file, "r")
+var_values = {}
+var_units = {}
+var_names = {}
+lst = list(dataset.variables.keys())
+print(lst)
